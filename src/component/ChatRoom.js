8b50c5b397ad39bfdf897ceba74d886dd1ddb412
sminy85 <seyang@presby.edu>
2020-09-15 23:36:29 +0900

    chatroom

--- src/component/ChatRoom.js
+++ src/component/ChatRoom.js
@@ -1,11 +1,55 @@
 import React from 'react'
-import '../css/base.css'
+import '../css/chatroom.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faArrowCircleUp,
+  faUser
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faUser,
+  faArrowCircleUp
+  // more icons go here
+)
 
 function ChatRoom () {
   return (
-    <div>
-      <p>채팅기능</p>
-    </div>
+      <div className='center_body6'>
+          <div className="header_cr">
+            <h3>도봉구 도봉산 등산</h3>
+          </div>
+          <div className="container">
+          <div className="opponent">
+            <p><FontAwesomeIcon icon={['fa', 'user']} size='2x' /></p>
+              크루장덕새
+            </div>
+              <p>안녕하세요~ 등산 크루에 오신걸 환영합니다.</p>
+              <span className="time-right">11:00</span>
+          </div>
+          <div className="container darker">
+            <p>안녕하세요^^</p>
+            <span className="time-left">11:01</span>
+          </div>
+          <div className="container">
+            <div className="opponent">
+            <p><FontAwesomeIcon icon={['fa', 'user']} size='2x' /></p>
+              크루장덕새
+            </div>
+              <p>궁금하신게 있으시면 언제든 말씀해주세요^^</p>
+              <span className="time-right">11:02</span>
+          </div>
+          <div className="container darker" id="myChat">
+            <p>만나는 장소가 정확히 어디인가요?</p>
+            <span className="time-left">11:05</span>
+          </div>
+          <div id="demo" />
+          <div className="typeBox">
+            <input id="inputBox" type="text" placeholder="메세지를 입력하세요" />
+            <FontAwesomeIcon icon={['fa', 'arrow-circle-up']} size='2x' onclick="myFunction()" />
+          </div>
+        </div>
   )
 }
 
--- src/component/Message.js
+++ src/component/Message.js
@@ -1,30 +1,12 @@
 import React from 'react'
-
 import '../css/base.css'
 import { NavLink } from 'react-router-dom'
 
-function Message() {
+function Message () {
   return (
-    <div className="message">
-
-<li><NavLink to='/chatroom' id="myTable" >정모방</NavLink></li> 
-
-        {/* <table id="myTable">
-          <tbody><tr>
-              <th style={{flexGrow: 3, textAlign: 'center'}}>Crew</th>
-              <th style={{flexGrow: 7, textAlign: 'center'}}>Name</th>
-            </tr>
-            <tr>
-              <td style={{flexGrow: 3}}><a href="chatroom.html">도봉구 도봉산 등산</a></td>
-              <td style={{flexGrow: 7}}><a href="chatroom.html">Berglunds snabbkop</a></td>
-            </tr>
-            <tr>
-              <td style={{flexGrow: 3}}><a href="chatroomVer2.html">마포대교 러닝크루</a></td>
-              <td style={{flexGrow: 7}}><a href="chatroomVer2.html">North/South</a></td>
-            </tr>
-          </tbody></table> */}
-      </div>
-
+    <div className="center_body6">
+      <li><NavLink to='/chatroom' id="myTable" >정모방</NavLink></li>
+    </div>
   )
 }
-export default Message;
+export default Message
