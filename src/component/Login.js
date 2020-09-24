10928396b58684c4d54dfb6a85cc550cc91eec36
sminy85 <smini857@naver.com>
2020-09-24 21:45:36 +0900

    use axios in login page

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import '../css/login.css'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
+
+function login (e) {
+  e.preventDefault();
+  const request = {
+    email: document.getElementById('exampleInputEmail1').value,
+    password: document.getElementById('exampleInputPassword1').value
+  }
+  axios.post('http://localhost:3000/login', request)
+    .then(resp => {
+      alert(resp.data.message);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
 
 function Login () {
   return (
@@ -14,14 +30,14 @@ function NewComponent3 () {
   return (
     <div className="main">
       <b><p className="logintitle">Login</p></b>
-      <form name="fo" className="login_form" method="get">
+      <form name="fo" className="login_form" method="get" onSubmit={(e) => login(e)}>
         <div className="loginfo_id">
-          <label><b><p id="un">Username</p></b></label>
-          <input type="text" size={20}/>
+          <label for='exampleInputEmail1'><b><p id="un">Username</p></b></label>
+          <input type="text" size={20} id='exampleInputEmail1' />
         </div>
         <div className="loginfo_pw">
-          <label htmlFor="pass"><b><p id="pw" >Password</p></b></label>
-          <input id="pass" type="password" size={20} defaultValue />
+          <label for='exampleInputPassword1'><b><p id="pw" >Password</p></b></label>
+          <input id='exampleInputPassword1' type="password" size={20} defaultValue />
         </div>
         <div className="login_btn">
           <input type="submit" Value="LOG IN" />
@@ -50,4 +66,5 @@ function NewComponent3 () {
     </div>
   )
 }
+
 export default Login
